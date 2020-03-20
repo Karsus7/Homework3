e82e7558441275c811478a1ab23d34977668d77a
William Broussard <wbroussard@me.com>
2020-03-19 21:53:34 -0500

    adjustments

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,27 @@
 var generateBtn = document.querySelector("#generate");
 
 
-var passLength = prompt("How long will your password be? It must be between 8 and 128 characetrs.");
+var passLength = prompt("How long will your password be? It must be between 8 and 128 characters.");
 var confirmCharacters = confirm("Would you like to include lowercase letters?");
 var confirmUppercase = confirm("Would you like to include uppercase letters?");
 var numberConfirm = confirm("Would you like to include number?");
 var specialConfirm = confirm("Would you like to include special characters?");
 
 function writePassword() {
-  var passLength = prompt("How long will your password be? It must be between 8 and 128 characetrs.");
-  console.log(passLength);
-  var lowerCharacters = prompt("Do you wish to include lowercase letters?");
-  console.log(lowerCharacters);
-  var upperCharacters = prompt("Do you wish to include uppercase letters?");
-  console.log(upperCharacters);
-  var numericCharacters = prompt("Do you wish to include numbers?");
-  console.log(numericCharacters);
-  var specialCharacters = prompt("Do you wish to include special characters?");
-  console.log(specialCharacters);
+
+  
+  if (lowerCharacters == true) {
+    generatePassword(lowerCharacters)
+  };
+  if (upperCharacters == true) {
+    generatePassword(upperCharacters)
+  };
+  if (numericCharacters == true) {
+    generatePassword(numericCharacters)
+  };
+  if (specialcharacters == true) {
+    generatePassword(specialCharacters)
+  };
 
   var password = generatePassword();
 
@@ -25,16 +29,11 @@ function writePassword() {
 
   passwordText.value = password;
 
+
 }
 
 
-
-
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', ')', '(', '-', '_', '=', '+', '`', '~', ',', '<', '.', '>', '/', '?', ';', ':', '\\'];
-var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var passLength = "";
+generateBtn.addEventListener("click".writePassword);
 
 
 // var password = generatePassword(numericCharacters, specialCharacters, lowerCharacters, upperCharacters, passLength);
@@ -43,32 +42,33 @@ var passLength = "";
 //   pass = "";
 
 
-
-
   function generatePassword() {
-
-    if (lowerCharacters == true) {
-      generatePassword(lowerCharacters)
-    };
-    if (upperCharacters == true) {
-      generatePassword(upperCharacters)
-    };
-    if (numericCharacters == true) {
-      generatePassword(numericCharacters)
-    };
-    if (specialcharacters == true) {
-      generatePassword(specialCharacters)
-    };
+    var passLength = prompt("How long will your password be? It must be between 8 and 128 characs.");
+    console.log(passLength);
+    var lowerCharacters = prompt("Do you wish to include lowercase letters?");
+    console.log(lowerCharacters);
+    var upperCharacters = prompt("Do you wish to include uppercase letters?");
+    console.log(upperCharacters);
+    var numericCharacters = prompt("Do you wish to include numbers?");
+    console.log(numericCharacters);
+    var specialCharacters = prompt("Do you wish to include special characters?");
+    console.log(specialCharacters);
   
-    generateBtn.addEventListener("click".writePassword);
+
+
 
     var password = "";
-    // for (var index = 1; index)
-      // console.log(password); index < parseInt(userPassLength); index++){
-      var character = Math.floor(Math.random() * (128 - 8))
+    // for (var index = 1; index);
+    //   console.log(password); index < parseInt(userPassLength); index++){
+      var password = Math.floor(Math.random() * (128 - 8)) + 8;
       password += numericCharacters[0], specialCharacters[0], lowerCharacters[0], upperCharacters[0];
     }
-    // console.log(password)
-    // return password;
-  // }
 
+    // return password;
+
+
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', ')', '(', '-', '_', '=', '+', '`', '~', ',', '<', '.', '>', '/', '?', ';', ':', '\\'];
+  var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var passLength = "";
