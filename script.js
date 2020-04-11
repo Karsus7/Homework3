6356292e2380d59434d956e82df3aa2118742fc3
William Broussard <wbroussard@me.com>
2020-04-11 14:13:43 -0500

    more comments added

--- script.js
+++ script.js
@@ -1,71 +1,78 @@
-document.querySelector("#generate").addEventListener("click", writePassword);
+var generateBtn = document.querySelector("#generate");
+//Arrays hold possible answers for prompts below.
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var characters = ["!", "@", "#", "$", "%", "^", "&", "*"]
+//"confirmed" variable refers to all used arrays selected in confirms.
+//"length" variable refers to the length of the password decided in the prompt.
+var confirmed = [];
+var length;
 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?", "~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-var confirmLength = "";
-var confirmSpecialCharacter;
-var confirmNumericCharacter;
-var confirmUpperCase;
-var confirmLowerCase;
-
-function generatePassword() {
-    var confirmLength = (prompt("How many characters would you like your password to contain?"));
-
-    while (confirmLength <= 7 || confirmLength >= 129) {
-        alert("Password length must be between 8-128 characters Try again");
-        var confirmLength = (prompt("How many characters would you like your password to contain?"));
-    }
-
-    alert(`Your password will have ${confirmLength} characters`);
-
-    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-    var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
-    var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-
-    while (confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
-        alert("You must choose at least one parameter");
-        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-        var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");
-        var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
-        var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");
-    }
-
-    var passwordCharacters = []
-
-    if (confirmSpecialCharacter) {
-        passwordCharacters = passwordCharacters.concat(specialChar)
-    }
-
-    if (confirmNumericCharacter) {
-        passwordCharacters = passwordCharacters.concat(number)
-    }
-
-    if (confirmLowerCase) {
-        passwordCharacters = passwordCharacters.concat(alphaLower)
-    }
-
-    if (confirmUpperCase) {
-        passwordCharacters = passwordCharacters.concat(alphaUpper)
-    }
-
-    console.log(passwordCharacters)
-
-    var randomPassword = ""
-
-    for (var i = 0; i < confirmLength; i++) {
-        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-        console.log(randomPassword)
-    }
-    return randomPassword;
-}
-
+//confirms provide yes or no questions, prompts provide an answer bar. Contains function for establishing password info.
 function writePassword() {
-    var password = generatePassword();
+    //"do" and "while" serves to run the "if" confirms, while certain conditons defined after "while" are met.
+    //"do" and "while" are essentially another form of "if" and "else".
+    do {
+        var useLowerCase = confirm("Do you your password to include lower case letters?")
+        var useUpperCase = confirm("Do you your password to include upper case letters?")
+        var useNumbers = confirm("Do you your password to include numbers?")
+        var useCharacters = confirm("Do you want your password to include special characters?")
+//Below lines insures users must use at least one type of written characters of those provided by the arrays.
+    if (useLowerCase == false && useUpperCase == false && useNumbers == false && useCharacters == false) {
+        alert("You must choose some type of input for your password.")
+    }
+    }
+    while (useLowerCase == false && useUpperCase == false && useNumbers == false && useCharacters == false)
+//Above "while" tells the prompts to appear if none of the options have been selected "yet", triggering the confirms.
+    do {
+        length = prompt("How long do you want your password to be? *Must be between 8-128*")
+    }
+//line below confirms that the input for length must be between 8 and 128.
+    while (length < 8 || length > 128 || isNaN(length));
+//"if" and "else" provide conditions. See 03-JavaScript Activity 35 index.html
+    if (useLowerCase == true) {
+//"concat" adds the arrays into the final product (the generated password).
+        confirmed = confirmed.concat(lowerCase);
+    } else (useLowerCase == false);
+//if the different types are confirmed they are dded to the total list of types used in the password.
+//if they are not confirmed then nothing happens and they are not added.
+    if (useUpperCase == true) {
+
+        confirmed = confirmed.concat(upperCase);
+    } else (useUpperCase == false);
+
+    if (useNumbers == true) {
+
+        confirmed = confirmed.concat(numbers);
+    } else (useNumbers == false);
+
+    if (useCharacters == true) {
+
+        confirmed = confirmed.concat(characters);
+    } else (useCharacters == false);
+
+
+//lines below actually make the password appear in index.html.
+    var password = generatePassword()
+
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
+
 }
+//Function takes info from writePassword and uses it to randomly generate a password.
+function generatePassword() {
+
+    var password = "";
+    for (var index = 0; index < parseInt(length); index++) {
+//for loop used for randomization of password  using the confirmed length and character type from the prompt and confirms.
+        var character = Math.floor(Math.random() * confirmed.length);
+        password += confirmed[character];
+
+    }
+//returns the finished password
+    return password;
+}
+// Adds an event listener to recognize the clicking of the generate password button.
+generateBtn.addEventListener("click", writePassword);
